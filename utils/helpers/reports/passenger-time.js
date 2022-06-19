d63da81f9681259d0c1c99b22029603981a4147b
John Bryan Lumanlan <101075331+trjohnbryanlumanlan@users.noreply.github.com>
2022-06-19 17:13:57 +0800

    convert 24hr to 12hr in passenger report by time

--- utils/helpers/reports/passenger-time.js
+++ utils/helpers/reports/passenger-time.js
@@ -1,6 +1,33 @@
 const { Bookings, LoopBusLocations, LoopBuses } = require('~/models');
 
 const { Op, Sequelize } = require("sequelize");
+
+const timeConversion = {
+  0: '12 AM',
+  1: '1 AM',
+  2: '2 AM',
+  3: '3 AM',
+  4: '4 AM',
+  5: '5 AM',
+  6: '6 AM',
+  7: '7 AM',
+  8: '8 AM',
+  9: '9 AM',
+  10: '10 AM',
+  11: '11 AM',
+  12: '12 PM',
+  13: '1 PM',
+  14: '2 PM',
+  15: '3 PM',
+  16: '4 PM',
+  17: '5 PM',
+  18: '6 PM',
+  19: '7 PM',
+  20: '8 PM',
+  21: '9 PM',
+  22: '10 PM',
+  23: '11 PM',
+}
 module.exports = async () => {
   let result = {};
 
@@ -85,11 +112,23 @@ module.exports = async () => {
       loopBus2Hours[time] = bookingsLoopBus2.length
     }
   
-  
+    let newLoopBus1Hours = {};
+    let newLoopBus2Hours = {};
+
+
+
+    Object.entries(loopBus1Hours).forEach(([k,v]) => {
+      newLoopBus1Hours[timeConversion[k]] = v
+    })
+
+    Object.entries(loopBus2Hours).forEach((k,v) => {
+      newLoopBus2Hours[timeConversion[k]] = v
+    })
+
 
     return {
-      loopBus1: loopBus1Hours,
-      loopBus2: loopBus2Hours
+      loopBus1: newLoopBus1Hours,
+      loopBus2: newLoopBus1Hours
     }
   }
  
